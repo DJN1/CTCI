95d05bf579b4abc4a46420e11af3ba1b7d5ded38
David Niederweis <david.niederweis@gmail.com>
2020-08-11 21:18:03 -0500

    added runtime for String Rotation

--- Chapter_1/String_Rotation/index.js
+++ Chapter_1/String_Rotation/index.js
@@ -1,3 +1,4 @@
+// Runs in O(N) time and O(1) space
 const bruteForce = (s1, s2) => {
   if (s1 === null || s2 === null || s1.length === 0 || s2.length === 0) return false;
   if (s1.length !== s2.length) return false;
